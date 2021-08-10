69ffd5dc7bdf665dcf9ed861dbc88ae46a99f709
onting <ghemool123@gmail.com>
2021-08-10 23:59:06 +0900

    Declare keyboard interface and implemention

--- hanjpkeyboard.h
+++ hanjpkeyboard.h
@@ -11,8 +11,19 @@ G_DECLARE_INTERFACE(HanjpKeyboard, hanjp_keyboard, HANJP, KEYBOARD, GObject)
 struct _HanjpKeyboardInterface {
     GTypeInterface parent_iface;
 
-    
+    gunichar (*get_mapping) (HanjpKeyboard *self, gint tableid, gint ascii);
 };
+
+gunichar hanjp_keyboard_get_mapping(HanjpKeyboard *self, gint tableid, gint ascii);
+
+#define HANJP_TYPE_KEYBOARDDEFAULT hanjp_keyboarddefault_get_type()
+G_DECLARE_DERIVABLE_TYPE(HanjpKeyboardDefault, hanjp_keyboarddefault, HANJP, KEYBOARDDEFAULT, GObject)
+
+struct _HanjpKeyboardDefaultClass {
+    GObjectClass parent_class;
+};
+
+HanjpKeyboardDefault *hanjp_keyboarddefault_new();
 
 G_END_DECLS
 
