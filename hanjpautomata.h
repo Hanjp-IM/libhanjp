6663af38a4b6359b4bcecc04b0e9391b5f4eac66
onting <ghemool123@gmail.com>
2021-08-10 20:43:42 +0900

    Design automata with GObject
    Declare enum, interface, implementing types.
    Rename prefix hanjp_automata to hanjp_am

--- hanjpautomata.h
+++ hanjpautomata.h
@@ -6,46 +6,47 @@
 
 G_BEGIN_DECLS
 
-typedef enum {
+enum {
     EAT,
     POP,
     FAIL
-} HanjpAmSig;
+};
 
-#define HANJP_TYPE_AUTOMATA hanjp_automata_get_type()
-G_DECLARE_INTERFACE(HanjpAutomata, hanjp_automata, HANJP, AUTOMATA, GObject)
+#define HANJP_TYPE_AUTOMATA hanjp_am_get_type()
+G_DECLARE_INTERFACE(HanjpAutomata, hanjp_am, HANJP, AUTOMATA, GObject)
 
 struct _HanjpAutomataInterface
 {
     GTypeInterface parant_iface;
 
-    HanjpAmSig (*push) (HanjpAutomata *self);
+    gboolean (*to_kana) (HanjpAutomata* self, GArray *dest, gunichar cho, gunichar jung, gunichar jung2, gunichar jong);
+    gint (*push) (HanjpAutomata *self, gunichar ch);
     gboolean (*backspace) (HanjpAutomata *self);
     gunichar (*flush) (HanjpAutomata *self);
 };
 
-gboolean hanjp_automata_to_kana(HanjpAutomata *am, GArray *dest, gunichar cho, gunichar jung, gunichar jung2, gunichar jong);
-HanjpAmSig hanjp_automata_push(HanjpAutomata *am);
-gboolean hanjp_automata_backspace(HanjpAutomata *am);
-gunichar hanjp_automata_flush(HanjpAutomata *am);
+gboolean hanjp_am_to_kana(HanjpAutomata *am, GArray *dest, gunichar cho, gunichar jung, gunichar jung2, gunichar jong);
+gint hanjp_am_push(HanjpAutomata *am, gunichar ch);
+gboolean hanjp_am_backspace(HanjpAutomata *am);
+gunichar hanjp_am_flush(HanjpAutomata *am);
 
-#define HANJP_TYPE_AUTOMATABASE hanjp_automatabase_get_type()
-G_DECLARE_DERIVABLE_TYPE(HanjpAutomataBase, hanjp_automatabase, HANJP, AUTOMATABASE, GObject)
+#define HANJP_TYPE_AUTOMATABASE hanjp_ambase_get_type()
+G_DECLARE_DERIVABLE_TYPE(HanjpAutomataBase, hanjp_ambase, HANJP, AUTOMATABASE, GObject)
 
 struct _HanjpAutomataBaseClass
 {
     GObjectClass parent_class;
 };
 
-#define HANJP_TYPE_AUTOMATADEFAULT hanjp_automatadefault_get_type()
-G_DECLARE_DERIVABLE_TYPE(HanjpAutomataDefault, hanjp_automatadefault, HANJP, AUTOMATADEFAULT, HanjpAutomataBase)
+#define HANJP_TYPE_AUTOMATADEFAULT hanjp_amdefault_get_type()
+G_DECLARE_DERIVABLE_TYPE(HanjpAutomataDefault, hanjp_amdefault, HANJP, AUTOMATADEFAULT, HanjpAutomataBase)
 
 struct _HanjpAutomataDefaultClass
 {
     HanjpAutomataBaseClass parent_class;
 };
 
-HanjpAutomataDefault *hanjp_automatadeafult_new();
+HanjpAutomataDefault *hanjp_amdefault_new();
 
 G_END_DECLS
 
