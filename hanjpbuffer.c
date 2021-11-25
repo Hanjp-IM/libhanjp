cf75f0ef393f238add9ad6f2778687c5af382db1
JaeSang Yoo <jsyoo5b@gmail.com>
2021-11-25 17:41:41 +0900

    refactor: simplify hanjpbuffer codes
    * Change magic nubmer (buffer size) into compile-time calculated const
    * `hanjp_buffer_push()`: simplify code flow
    * `hanjp_buffer_pop()`: refactor return buffer value
    
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

--- hanjpbuffer.c
+++ hanjpbuffer.c
@@ -6,51 +6,40 @@
 extern ucschar hangul_choseong_to_jongseong(ucschar c);
 extern ucschar hangul_jongseong_to_choseong(ucschar c);
 
+static const int BUFF_SIZE =
+	(sizeof(((HanjpBuffer*)0)->stack) / sizeof(((HanjpBuffer*)0)->stack[0]));
+
 /* HanjpBuffer Implementation */
 gunichar
 hanjp_buffer_push(HanjpBuffer *buffer, gunichar ch) {
-    if(hangul_is_choseong(ch)) {
-        if(buffer->cho == 0) {
-            buffer->cho = ch;
-        }
-        else {
-            return ch;
-        }
+    if ((hangul_is_choseong(ch)) && (buffer->cho == 0)) {
+        buffer->cho = ch;
+		return 0;
     }
-    else if(hangul_is_jungseong(ch)) {
+    else if (hangul_is_jungseong(ch)) {
         if(buffer->jung == 0) {
             buffer->jung = ch;
+			return 0;
         }
-        else {
-            if(buffer->jung2 == 0) {
-                buffer->jung2 = ch;
-            }
-            else {
-                return ch;
-            }
+        else if(buffer->jung2 == 0) {
+            buffer->jung2 = ch;
+			return 0;
         }
     }
-    else if(hangul_is_jongseong(ch)) {
-        if(buffer->jong == 0) {
-            buffer->jong = ch;
-        }
-        else {
-            return ch;
-        }
-    }
-    else {
-        return ch;
+    else if ((hangul_is_jongseong(ch)) && (buffer->jong == 0)) {
+        buffer->jong = ch;
+		return 0;
     }
 
-    return 0;
+    return ch;
 }
 
 gint
 hanjp_buffer_peek(HanjpBuffer *buffer) {
-    int i;
+	int i;
 
-    for(i = 0; i < 4; i++) {
-        if(buffer->stack[i] != 0) {
+    for (i = 0; i < BUFF_SIZE; i++) {
+        if (buffer->stack[i]) {
             return buffer->stack[i];
         }
     }
@@ -60,18 +49,18 @@ hanjp_buffer_peek(HanjpBuffer *buffer) {
 
 gunichar
 hanjp_buffer_pop(HanjpBuffer *buffer) {
+	gint ret = 0;
     int i;
-    gunichar r;
 
-    for(i = 3; i >= 0; i--) {
-        r = buffer->stack[i];
-        if(r != 0) {
+    for (i = BUFF_SIZE-1; i > -1; i--) {
+        if (buffer->stack[i]) {
+			ret = buffer->stack[i];
             buffer->stack[i] = 0;
             break;
         }
     }
 
-    return r;
+    return ret;
 }
 
 void
